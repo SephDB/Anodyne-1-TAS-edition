44270d0c18344f2ce7af9ccba4444667bc601e8d
Seph De Busser <seph.debusser@gmail.com>
2022-10-06 16:54:30 +0200

    Initial setup

--- intra/bat/SetupSDK.bat
+++ intra/bat/SetupSDK.bat
@@ -1,7 +1,7 @@
 :user_configuration
 
 :: Path to Flex SDK
-set FLEX_SDK=C:\Users\hantani\Documents\Anodyne 1 Repo\installation files\flex_sdk_4.6
+set FLEX_SDK=D:\Projects_Seph\Anodyne-1-Repo\installation files\flex_sdk_4.6
 
 
 :validation
